632e2fe34592d690fe3ceabb66c27cc64c208d22
Tiffane <zohoungbogboanlyou@gmail.com>
2022-11-16 04:12:13 +0100

    return 0 on node NULL

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -39,6 +39,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int h_left, h_right;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree->left)
 		h_left = 1 + binary_tree_height(tree->left);
 	else
