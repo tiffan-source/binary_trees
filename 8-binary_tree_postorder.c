8baedc505c79855c381374530fb366b6ff51c493
Tiffane <zohoungbogboanlyou@gmail.com>
2022-11-16 01:46:49 +0100

    fix tsk 7

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -14,9 +14,9 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		if (tree->left != NULL)
 			binary_tree_inorder(tree->left, func);
 
-		func(tree->n);
-
 		if (tree->right != NULL)
 			binary_tree_inorder(tree->right, func);
+
+		func(tree->n);
 	}
 }
